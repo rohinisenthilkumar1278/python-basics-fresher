cd4eb7b159d94a4558298db72d91b46166906710
rohinisenthilkumar1278 <rohinisenthilkumar1278@gmail.com>
2026-01-19 20:38:43 +0530

    Add print_basics.py

---
+++ print_basics.py
@@ -0,0 +1,10 @@
+print("Rohini")
+print("Chettinad College of Engineering and Technology, Karur")
+print("Department: IT")
+
+print("1 * 5 = 5")
+
+print("*")
+print("**")
+print("***")
+print("****")
