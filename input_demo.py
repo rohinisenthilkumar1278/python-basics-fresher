40905f592413ccb27fe55542ec950e992f44a47d
rohinisenthilkumar1278 <rohinisenthilkumar1278@gmail.com>
2026-01-19 20:40:57 +0530

    Add input_demo.py

---
+++ input_demo.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("First number:", a)
+print("Second number:", b)
+
+name = input("Enter your name: ")
+print(name, "Good Morning")
+
+marks = int(input("Enter marks: "))
+print(marks, "passed")
