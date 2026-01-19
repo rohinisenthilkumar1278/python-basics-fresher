83c2244247fe802968ce45b50e5b5dd522093951
rohinisenthilkumar1278 <rohinisenthilkumar1278@gmail.com>
2026-01-19 20:41:48 +0530

    Add type_conversion.py

---
+++ type_conversion.py
@@ -0,0 +1,14 @@
+a = float(10)
+print(a)
+
+b = int(5.8)
+print(b)
+
+c = str(100)
+print(c)
+
+x = int(input("Enter a number: "))
+y = float(input("Enter a decimal number: "))
+
+print("Integer value:", x)
+print("Float value:", y)
